b66f6febcabd62b0119d4e319bca898e3dcf72a5
zulkarnay <bedelbaev@gmail.com>
2018-01-23 13:44:54 +0600

    add 'status' method in invites decorator

--- app/decorators/invite_decorator.rb
+++ app/decorators/invite_decorator.rb
@@ -4,4 +4,8 @@ class InviteDecorator < ApplicationDecorator
   decorates_association :user
   decorates_association :assessment
   decorates_association :feedback
+
+  def status
+    invite.feedback.present? ? "Заполнил(а) отзыв" : "В процессе"
+  end
 end
