a1c27fe91b34ebb4b9dcc50e2426156ee1532893
zulkarnay <bedelbaev@gmail.com>
2018-02-25 22:21:02 +0600

    add default value when ENV var not present

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -275,5 +275,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.omniauth :google_oauth2, ENV.fetch("GOOGLE_CLIENT_ID"), ENV.fetch("GOOGLE_CLIENT_SECRET")
+  config.omniauth :google_oauth2, ENV.fetch("GOOGLE_CLIENT_ID", ""), ENV.fetch("GOOGLE_CLIENT_SECRET", "")
 end
